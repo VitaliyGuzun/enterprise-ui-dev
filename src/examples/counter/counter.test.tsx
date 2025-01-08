cef2af9e5beecf3a6806456046f77642acf147ed
VitaliyGuzun <vitaliy@miro.com>
2025-01-08 22:06:11 +0100

    Refactor

--- src/examples/counter/counter.test.tsx
+++ src/examples/counter/counter.test.tsx
@@ -1,7 +1,7 @@
 // @vitest-environment happy-dom
 
-import { screen, render, fireEvent } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import { render } from './test/utilities';
 import Counter from '.';
 
 test('it should render the component', () => {
@@ -9,13 +9,13 @@ test('it should render the component', () => {
 });
 
 test('it should increment when the "Increment" button is pressed', async () => {
-  render(<Counter />);
+  const { user } = render(<Counter />);
   const currentCount = screen.getByTestId('current-count');
 
   expect(currentCount).toHaveTextContent('0');
 
   const button = screen.getByRole('button', { name: 'Increment' });
-  fireEvent.click(button);
+  await user.click(button);
 
   expect(currentCount).toHaveTextContent('1');
 });
